7e8d6773cb3a0cd71ae5ce94eda4b5b69c901c32
andrea fortuna <andrea.fortuna@iit.it>
2023-04-14 18:51:13 +0200

    Added homing task.

--- src/main_fsm.cpp
+++ src/main_fsm.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "hrii_task_board_fsm/PressButton.h"
 #include "hrii_task_board_fsm/MoveSlider.h"
+#include "hrii_task_board_fsm/Homing.h"
 #include <tf2_ros/transform_listener.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <controller_manager_msgs/ListControllers.h>
@@ -10,6 +11,9 @@ class MainFSM
     public:
         MainFSM() : nh_priv_("~")
         {
+            homing_service_name_ = "homing_fsm/activate";
+            homing_client_ = nh_.serviceClient<hrii_task_board_fsm::Homing>(homing_service_name_);
+
             press_button_activation_service_name_ = "press_button_fsm/activate";
             press_button_activation_client_ = nh_.serviceClient<hrii_task_board_fsm::PressButton>(press_button_activation_service_name_);
 
@@ -61,6 +65,9 @@ class MainFSM
             }while(!controller_started_flag);
 
             // Wait for task services activation
+            ROS_INFO_STREAM("Waiting for " << nh_.resolveName(homing_service_name_) << " ROS service...");
+            homing_client_.waitForExistence();
+
             ROS_INFO_STREAM("Waiting for " << nh_.resolveName(press_button_activation_service_name_) << " ROS service...");
             press_button_activation_client_.waitForExistence();
 
@@ -78,7 +85,11 @@ class MainFSM
                 {
                     // Move to homing pose and wait for start
                     ROS_INFO("- - - HOMING STATE - - -");
-                    state_ = MainFSM::states::BOARD_DETECTION;
+                    if (homing())
+                        state_ = MainFSM::states::BOARD_DETECTION;
+                    else
+                        state_ = MainFSM::states::ERROR;
+                        // or PRESS_RED_BUTTON
                     break;
                 }
                     
@@ -161,6 +172,9 @@ class MainFSM
         std::string left_robot_controller_manager_status_service_name_;
         ros::ServiceClient left_robot_controller_manager_status_client_;
 
+        std::string homing_service_name_;
+        ros::ServiceClient homing_client_;
+
         std::string press_button_activation_service_name_;
         ros::ServiceClient press_button_activation_client_;
 
@@ -185,6 +199,38 @@ class MainFSM
                             EXIT,
                             ERROR} state_;
 
+        bool homing()
+        {
+            ROS_INFO("Go to home position...");
+
+            hrii_task_board_fsm::Homing homing_srv;
+            homing_srv.request.robot_id = left_robot_id_;
+
+            // Home pose
+            geometry_msgs::Pose home_pose;
+            home_pose.position.x = 0.383;
+            home_pose.position.y = -0.253;
+            home_pose.position.z = 0.379;
+            home_pose.orientation.x = -0.693;
+            home_pose.orientation.y = 0.706;
+            home_pose.orientation.z = -0.104;
+            home_pose.orientation.w = -0.104;
+            homing_srv.request.home_pose.pose = home_pose;
+
+            if (!homing_client_.call(homing_srv))
+            {
+                ROS_ERROR("Error calling homing service.");
+                return false;
+            }
+            else if (!homing_srv.response.success)
+            {
+                ROS_ERROR("Failure going home. Exiting.");
+                return false;
+            }
+            ROS_INFO("Homing succeded.");
+            return true;
+        }
+
         bool pressRedButton()
         {
             ROS_INFO("Pressing the red button...");
@@ -193,38 +239,38 @@ class MainFSM
             press_button_srv.request.robot_id = left_robot_id_;
 
             // Fake button pose
-            // geometry_msgs::Pose fake_button_pose;
-            // fake_button_pose.position.x = 0.3;
-            // fake_button_pose.position.y = 0.0;
-            // fake_button_pose.position.z = 0.3;
-            // fake_button_pose.orientation.x = 1.0;
-            // fake_button_pose.orientation.y = 0.0;
-            // fake_button_pose.orientation.z = 0.0;
-            // fake_button_pose.orientation.w = 0.0;
-            // press_button_srv.request.button_pose.pose = fake_button_pose;
+            geometry_msgs::Pose fake_button_pose;
+            fake_button_pose.position.x = 0.3;
+            fake_button_pose.position.y = 0.0;
+            fake_button_pose.position.z = 0.3;
+            fake_button_pose.orientation.x = 1.0;
+            fake_button_pose.orientation.y = 0.0;
+            fake_button_pose.orientation.z = 0.0;
+            fake_button_pose.orientation.w = 0.0;
+            press_button_srv.request.button_pose.pose = fake_button_pose;
 
             // Real button pose
-            geometry_msgs::TransformStamped redButtonTransform;
-            try{
-                redButtonTransform = tfBuffer.lookupTransform("franka_left_link0", "task_board_red_button_link", ros::Time(0), ros::Duration(3));
-                ROS_INFO("Tranform btw franka_left_link0 and task_board_red_button_link found!");
-            }
-            catch (tf2::TransformException &ex) {
-                ROS_WARN("%s",ex.what());
-                ros::Duration(1.0).sleep();
-                ROS_ERROR("Tranform btw franka_left_link0 and task_board_red_button_link NOT found!");
-                return false;
-            }
-            geometry_msgs::Pose red_button_pose;
-            red_button_pose.orientation = redButtonTransform.transform.rotation;
-            red_button_pose.position.x = redButtonTransform.transform.translation.x;
-            red_button_pose.position.y = redButtonTransform.transform.translation.y;
-            red_button_pose.position.z = redButtonTransform.transform.translation.z;
+            // geometry_msgs::TransformStamped redButtonTransform;
+            // try{
+            //     redButtonTransform = tfBuffer.lookupTransform("franka_left_link0", "task_board_red_button_link", ros::Time(0), ros::Duration(3));
+            //     ROS_INFO("Tranform btw franka_left_link0 and task_board_red_button_link found!");
+            // }
+            // catch (tf2::TransformException &ex) {
+            //     ROS_WARN("%s",ex.what());
+            //     ros::Duration(1.0).sleep();
+            //     ROS_ERROR("Tranform btw franka_left_link0 and task_board_red_button_link NOT found!");
+            //     return false;
+            // }
+            // geometry_msgs::Pose red_button_pose;
+            // red_button_pose.orientation = redButtonTransform.transform.rotation;
+            // red_button_pose.position.x = redButtonTransform.transform.translation.x;
+            // red_button_pose.position.y = redButtonTransform.transform.translation.y;
+            // red_button_pose.position.z = redButtonTransform.transform.translation.z;
 
-            ROS_INFO_STREAM("Red button pose: " << red_button_pose.position.x << ", " << red_button_pose.position.y << ", " << red_button_pose.position.z);
-            ROS_INFO_STREAM("Red button orientation: " << red_button_pose.orientation.x << ", " << red_button_pose.orientation.y << ", " << red_button_pose.orientation.z << ", " << red_button_pose.orientation.w);
+            // ROS_INFO_STREAM("Red button pose: " << red_button_pose.position.x << ", " << red_button_pose.position.y << ", " << red_button_pose.position.z);
+            // ROS_INFO_STREAM("Red button orientation: " << red_button_pose.orientation.x << ", " << red_button_pose.orientation.y << ", " << red_button_pose.orientation.z << ", " << red_button_pose.orientation.w);
 
-            press_button_srv.request.button_pose.pose = red_button_pose;
+            // press_button_srv.request.button_pose.pose = red_button_pose;
 
             if (!press_button_activation_client_.call(press_button_srv))
             {
@@ -254,7 +300,7 @@ class MainFSM
             fake_button_pose.orientation.y = 0.0;
             fake_button_pose.orientation.z = 0.0;
             fake_button_pose.orientation.w = 0.0;
-            move_slider_srv.request.button_pose.pose = fake_button_pose;
+            move_slider_srv.request.slider_pose.pose = fake_button_pose;
 
             if (!move_slider_activation_client_.call(move_slider_srv))
             {
